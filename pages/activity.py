43ce93d513681d03aafb44759e50a081b69f2c4d
mary-lev <marylevchenko@gmail.com>
2023-10-24 23:35:55 +0200

    Add line chart

--- pages/activity.py
+++ pages/activity.py
@@ -1,27 +1,27 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title="Particiation",
     page_icon="👋",
 )
 
-st.write("# Exploratory Data Analysis: Events' Participation 👋")
+st.write("# Events' Frequency")
 
+df = pd.read_csv("data/event_frequency.csv")
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+# Assuming 'date' column is of string type, convert it to datetime
+df['date'] = pd.to_datetime(df['date'])
+
+# Now set 'date' as the index
+df.set_index('date', inplace=True)
+
+yearly_events = df.resample('Y').size()
+
+# Convert to DataFrame
+yearly_events_df = yearly_events.reset_index(name='event_count')
+
+# Set 'date' as the index again
+yearly_events_df.set_index('date', inplace=True)
+
+st.line_chart(yearly_events_df)
